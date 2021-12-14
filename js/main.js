4fc01f17ff66eb16676b12376385d6b059e6e728
pd1116 <70820225+pd1116@users.noreply.github.com>
2021-12-14 15:56:34 +0530

    dynamic slide show

--- js/main.js
+++ js/main.js
@@ -1,37 +1,34 @@
+var nextSlide = document.querySelector("#nextBtn");
+var prevSlide = document.querySelector("#prevBtn");
+var images = [
+  "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1583121274602-3e2820c69888?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+];
 
-var slides=document.querySelector('.slider-items').children;
-var nextSlide=document.querySelector("#nextBtn");
-var prevSlide=document.querySelector("#prevBtn");
-var totalSlides=slides.length;
-var counter=0;
+var i = 0;
 
-nextSlide.onclick=function () {
-     next("next");
+function slideShow() {
+  document.getElementById("image").src = images[i];
 }
-prevSlide.onclick=function () {
-     next("prev");
-}
 
-function next(direction){
-
-   if(direction=="next"){
-      counter++;
-       if(counter==totalSlides){
-        counter=0;
-       }
-   } 
-   else{
-           if(counter==0){
-            counter=totalSlides-1;
-           }
-           else{
-            counter--;
-           }
-    }
-
-  for(i=0;i<slides.length;i++){
-          slides[i].classList.remove("active");
+nextSlide.onclick = function () {
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
   }
-  slides[counter].classList.add("active");     
+  document.getElementById("image").src = images[i];
+};
+prevSlide.onclick = function () {
+  if (i == 0) {
+    i = images.length - 1;
+  } else {
+    i--;
+  }
+  document.getElementById("image").src = images[i];
+};
 
-}
+window.onload = slideShow;
